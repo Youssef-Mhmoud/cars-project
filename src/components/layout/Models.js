b09bd157341fbd22bde7f947769cd7383e9e7f71
Youssef_Mahmoud <100860879+Youssef-Mhmoud@users.noreply.github.com>
2023-05-10 20:40:39 +0300

    minify imgs

--- src/components/layout/Models.js
+++ src/components/layout/Models.js
@@ -5,29 +5,75 @@ import { Link } from "react-router-dom";
 const Models = () => {
   return (
     <section className={classes.modelsSection}>
-      <img
-        src={require("../../assets/cars-model/Car-1.png")}
-        className={classes.imgHidden}
-        alt=""
-      />
-      {/* <picture>
-                        <source srcSet={item.optImg} type="image/webp" />
-                        <source srcSet={item.img} type="image/png" />
-                        <img src={item.img} alt="img" className="img1" />
-                      </picture> */}
-      <img src={require("../../assets/cars-model/Car-2.png")} alt="" />
-      <img
-        src={require("../../assets/cars-model/Car-3.png")}
-        className={classes.imgHidden}
-        alt=""
-      />
-      <img
-        src={require("../../assets/cars-model/Car-4.png")}
-        className={classes.imgHidden}
-        alt=""
-      />
+      <picture>
+        <source
+          srcSet={require("../../assets/cars-model/Car-1.webp")}
+          type="image/webp"
+        />
+        <source
+          srcSet={require("../../assets/cars-model/Car-1.png")}
+          type="image/png"
+        />
+        <img
+          src={require("../../assets/cars-model/Car-1.png")}
+          className={classes.imgHidden}
+          alt=""
+        />
+      </picture>
+      <picture>
+        <source
+          srcSet={require("../../assets/cars-model/Car-2.webp")}
+          type="image/webp"
+        />
+        <source
+          srcSet={require("../../assets/cars-model/Car-2.png")}
+          type="image/png"
+        />
+        <img src={require("../../assets/cars-model/Car-2.png")} alt="" />
+      </picture>
+      <picture>
+        <source
+          srcSet={require("../../assets/cars-model/Car-3.webp")}
+          type="image/webp"
+        />
+        <source
+          srcSet={require("../../assets/cars-model/Car-3.png")}
+          type="image/png"
+        />
+        <img
+          src={require("../../assets/cars-model/Car-3.png")}
+          className={classes.imgHidden}
+          alt=""
+        />
+      </picture>
+      <picture>
+        <source
+          srcSet={require("../../assets/cars-model/Car-4.webp")}
+          type="image/webp"
+        />
+        <source
+          srcSet={require("../../assets/cars-model/Car-4.png")}
+          type="image/png"
+        />
+        <img
+          src={require("../../assets/cars-model/Car-4.png")}
+          className={classes.imgHidden}
+          alt=""
+        />
+      </picture>
+
       <div className={classes.imgInfo}>
-        <img src={require("../../assets/cars-model/Car-5.png")} alt="" />
+        <picture>
+          <source
+            srcSet={require("../../assets/cars-model/Car-5.webp")}
+            type="image/webp"
+          />
+          <source
+            srcSet={require("../../assets/cars-model/Car-5.png")}
+            type="image/png"
+          />
+          <img src={require("../../assets/cars-model/Car-5.png")} alt="" />
+        </picture>
         <div>
           <h3>Tesla Model 3</h3>
           <p>Disruptive, avant-garde, futuristic, innovative.</p>
@@ -39,22 +85,65 @@ const Models = () => {
           </Link>
         </div>
       </div>
-      <img
-        src={require("../../assets/cars-model/Car-6.png")}
-        className={classes.imgHidden}
-        alt=""
-      />
-      <img
-        src={require("../../assets/cars-model/Car-7.png")}
-        alt=""
-        className={classes.imgHidden}
-      />
-      <img src={require("../../assets/cars-model/Car-8.png")} alt="" />
-      <img
-        src={require("../../assets/cars-model/Car-9.png")}
-        alt=""
-        className={classes.imgHidden}
-      />
+      <picture>
+        <source
+          srcSet={require("../../assets/cars-model/Car-6.webp")}
+          type="image/webp"
+        />
+        <source
+          srcSet={require("../../assets/cars-model/Car-6.png")}
+          type="image/png"
+        />
+        <img
+          src={require("../../assets/cars-model/Car-6.png")}
+          className={classes.imgHidden}
+          alt=""
+        />
+      </picture>
+      <picture>
+        <source
+          srcSet={require("../../assets/cars-model/Car-7.webp")}
+          type="image/webp"
+        />
+        <source
+          srcSet={require("../../assets/cars-model/Car-7.png")}
+          type="image/png"
+        />
+        <img
+          src={require("../../assets/cars-model/Car-7.png")}
+          className={classes.imgHidden}
+          alt=""
+        />
+      </picture>
+      <picture>
+        <source
+          srcSet={require("../../assets/cars-model/Car-8.webp")}
+          type="image/webp"
+        />
+        <source
+          srcSet={require("../../assets/cars-model/Car-8.png")}
+          type="image/png"
+        />
+
+        <img src={require("../../assets/cars-model/Car-8.png")} alt="" />
+      </picture>
+
+      <picture>
+        <source
+          srcSet={require("../../assets/cars-model/Car-9.webp")}
+          type="image/webp"
+        />
+        <source
+          srcSet={require("../../assets/cars-model/Car-9.png")}
+          type="image/png"
+        />
+
+        <img
+          src={require("../../assets/cars-model/Car-9.png")}
+          alt=""
+          className={classes.imgHidden}
+        />
+      </picture>
     </section>
   );
 };
